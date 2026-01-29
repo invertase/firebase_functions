c23eb126182161f9b10da532a479849d62050a21
Moritz <mosum@google.com>
2026-01-29 18:00:22 +0100

    add example

---
+++ firebase_functions_genkit/example/bin/example.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_functions/firebase_functions.dart';
+import 'package:firebase_functions_genkit/firebase_functions_genkit.dart';
+import 'package:genkit/genkit.dart';
+import 'package:genkit_google_genai/genkit_google_genai.dart';
+import 'package:schemantic/schemantic.dart';
+
+const name = 'jokeTeller';
+
+void main(List<String> args) {
+  final gemini = googleAI();
+  final ai = Genkit(plugins: [gemini]);
+  Flow<String, String, String, Object> flow = ai.defineFlow(
+    name: name,
+    inputType: stringType(),
+    outputType: stringType(),
+    streamType: stringType(),
+    fn: (jokeType, context) async {
+      final prompt = 'Tell me a $jokeType joke.';
+      final stream = ai.generateStream(
+        model: gemini.model('gemini-2.5-flash'),
+        prompt: prompt,
+      );
+      await stream.forEach((chunk) => context.sendChunk(chunk.text));
+      return stream.result.text;
+    },
+  );
+
+  fireUp(args, (firebase) {
+    firebase.https.onCallGenkit(name: name, flow: flow);
+  });
+}
--- firebase_functions_genkit/lib/src/firebase_functions_genkit_base.dart
+++ firebase_functions_genkit/lib/src/firebase_functions_genkit_base.dart
@@ -1,26 +1,26 @@
 import 'package:firebase_functions/firebase_functions.dart';
-import 'package:firebase_functions/src/firebase.dart';
-import 'package:genkit/client.dart' show ActionStream;
-import 'package:genkit/src/core/flow.dart';
+import 'package:genkit/genkit.dart';
 // ignore: experimental_member_use
 import 'package:meta/meta.dart' show mustBeConst;
 
 extension GenkitExt on HttpsNamespace {
-  void onCallGenkit<Output extends Object, Schema, Init>(
+  void onCallGenkit<Output extends Object, Schema, Init>({
     // Must repeat the name
     // ignore: experimental_member_use
-    @mustBeConst String name,
-    Flow<Object?, Output, Schema, Init> flow,
-  ) {
+    @mustBeConst required String name,
+    required Flow<Object?, Output, Schema, Init> flow,
+    // ignore: experimental_member_use
+    @mustBeConst CallableOptions? options = const CallableOptions(),
+  }) {
     // ignore: non_const_argument_for_const_parameter
-    onCall(name: name, (request, response) async {
-      if (!request.acceptsStreaming) {
+    onCall(name: name, options: options, (request, response) async {
+      if (request.acceptsStreaming) {
+        final actionStream = flow.stream(request.data);
+        actionStream.forEach((chunk) => response.sendChunk(chunk));
+        return CallableResult(actionStream.result);
+      } else {
         return CallableResult(await flow.run(request.data));
       }
-
-      final actionStream = flow.stream(request.data);
-      actionStream.forEach((chunk) => response.sendChunk(chunk));
-      return CallableResult(actionStream.result);
     });
   }
 }
