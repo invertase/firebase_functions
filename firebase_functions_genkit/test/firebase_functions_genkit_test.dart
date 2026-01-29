0401c963de8d87f8779fefc539389b1fb1579902
Moritz <mosum@google.com>
2026-01-29 16:21:03 +0100

    initial

---
+++ firebase_functions_genkit/test/firebase_functions_genkit_test.dart
@@ -0,0 +1,16 @@
+import 'package:firebase_functions_genkit/firebase_functions_genkit.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
