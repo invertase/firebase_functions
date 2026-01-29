f5106545c024207f41f243e5f790bb656e92c10d
Moritz <mosum@google.com>
2026-01-29 18:00:22 +0100

    add example

--- firebase_functions_genkit/example/firebase_functions_genkit_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:firebase_functions_genkit/firebase_functions_genkit.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
--- firebase_functions_genkit/test/firebase_functions_genkit_test.dart
+++ firebase_functions_genkit/test/firebase_functions_genkit_test.dart
@@ -1,16 +1,11 @@
-import 'package:firebase_functions_genkit/firebase_functions_genkit.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
-    final awesome = Awesome();
-
     setUp(() {
       // Additional setup goes here.
     });
 
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
+    test('First Test', () {});
   });
 }
