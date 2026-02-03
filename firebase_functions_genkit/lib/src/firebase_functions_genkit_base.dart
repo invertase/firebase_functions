1986765ad3d0efe212645b780d849a190a75d975
Moritz <mosum@google.com>
2026-02-03 14:58:09 +0100

    add contextprovider

--- firebase_functions_genkit/lib/src/firebase_functions_genkit_base.dart
+++ firebase_functions_genkit/lib/src/firebase_functions_genkit_base.dart
@@ -11,15 +11,21 @@ extension GenkitExt on HttpsNamespace {
     required Flow<Object?, Output, Schema, Init> flow,
     // ignore: experimental_member_use
     @mustBeConst CallableOptions? options = const CallableOptions(),
+    Map<String, dynamic> Function(CallableRequest<Object?>)? contextProvider,
   }) {
     // ignore: non_const_argument_for_const_parameter
     onCall(name: name, options: options, (request, response) async {
       if (request.acceptsStreaming) {
-        final actionStream = flow.stream(request.data);
+        final actionStream = flow.stream(
+          request.data,
+          context: contextProvider?.call(request),
+        );
         actionStream.forEach((chunk) => response.sendChunk(chunk));
         return CallableResult(actionStream.result);
       } else {
-        return CallableResult(await flow.run(request.data));
+        return CallableResult(
+          await flow.run(request.data, context: contextProvider?.call(request)),
+        );
       }
     });
   }
